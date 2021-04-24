98308ce12e2c6d96e941fe3dd82c832a01fb6ebd
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-04-23 23:48:56 -0600

    adding inertia for renewables and storage

--- src/device_models/devices/common/constraints_structs.jl
+++ src/device_models/devices/common/constraints_structs.jl
@@ -5,15 +5,52 @@ struct ServiceRampConstraintInfo <: PSI.AbstractRampConstraintInfo
     ramp_limits::PSI.UpDown
 end
 
-struct InertiaServiceConstraintInfo <: AbstractServiceConstraintInfo
+struct InertiaServiceConstraintInfo <: PSI.AbstractRangeConstraintInfo
     component_name::String
-    inertia::Float64
-    base_power::Float64
+    h_constant::Float64
+    limit::Float64
+    additional_terms_ub::Vector{Symbol}
+    additional_terms_lb::Vector{Symbol}
+end
+
+function InertiaServiceConstraintInfo(name::String, h_constant::Float64, limit::Float64)
+    return InertiaServiceConstraintInfo(
+        name,
+        h_constant,
+        limit,
+        Vector{Symbol}(),
+        Vector{Symbol}(),
+    )
+end
+
+struct InertiaServiceRenewableConstraintInfo <: PSI.AbstractRangeConstraintInfo
+    component_name::String
+    h_constant::Float64
+    multiplier::Float64
+    timeseries::Vector{Float64}
+    additional_terms_ub::Vector{Symbol}
+    additional_terms_lb::Vector{Symbol}
+end
+
+function InertiaServiceRenewableConstraintInfo(
+    name::String,
+    h_constant::Float64,
+    multiplier::Float64,
+    timeseries::Vector{Float64},
+)
+    return InertiaServiceRenewableConstraintInfo(
+        name,
+        h_constant,
+        multiplier,
+        timeseries,
+        Vector{Symbol}(),
+        Vector{Symbol}(),
+    )
 end
 
 struct InertiaCommitmentConstraintInfo <: AbstractServiceConstraintInfo
     component_name::String
     ic_power::PSI.InitialCondition
-    inertia::Float64
+    h_constant::Float64
     base_power::Float64
 end
---
+++ src/device_models/devices/storage.jl
@@ -0,0 +1,41 @@
+struct BookKeepingwInertia <: PSI.AbstractStorageFormulation end
+
+function inertia_constraints!(
+    optimization_container::PSI.OptimizationContainer,
+    devices::IS.FlattenIteratorWrapper{T},
+    model::PSI.DeviceModel{T, D},
+    ::Type{S},
+    feedforward::Union{Nothing, PSI.AbstractAffectFeedForward},
+) where {T <: PSY.Storage, D <: BookKeepingwInertia, S <: PM.AbstractPowerModel}
+    if _has_inertia_service(model)
+        service_model = _get_inertia_service_model(model)
+        service = _get_services(first(devices), service_model)[1]
+
+        time_steps = PSI.model_time_steps(optimization_container)
+        use_parameters = PSI.model_has_parameters(optimization_container)
+        constraint_info = Vector{InertiaServiceConstraintInfo}(undef, length(devices))
+        for (idx, d) in enumerate(devices)
+            constraint_info[idx] = InertiaServiceConstraintInfo(
+                PSY.get_name(d),
+                _get_inertia(d),
+                PSY.get_output_active_power_limits(d).max,
+            )
+            PSI.add_device_services!(constraint_info[idx], d, model)
+        end
+
+        storage_device_inertia!(
+            optimization_container,
+            constraint_info,
+            (
+                PSI.make_constraint_name(INERTIA_LIMIT, T),
+                PSI.make_constraint_name(INERTIA_ENERGY_LIMIT, T),
+            ),
+            (
+                PSI.make_variable_name(PSI.ACTIVE_POWER_OUT, T),
+                PSI.make_variable_name(PSI.ENERGY, T),
+            ),
+            PSI.make_variable_name(PSY.get_name(service), typeof(service)),
+        )
+    end
+    return
+end
